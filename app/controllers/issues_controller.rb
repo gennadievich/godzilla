ba07ec40703fa02257c6983e3965e42ee55d5833
Kirill Kovalenko <kkovalenko@beltechsoftware.com>
2016-06-01 13:48:36 +0300

    #3 Create 'Visit new issue path and fill in form' spec and make it pass.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,4 +2,12 @@ class IssuesController < ApplicationController
   def index
     @issues = Issue.all
   end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+
+  end
 end
--- spec/features/issues_spec.rb
+++ spec/features/issues_spec.rb
@@ -11,5 +11,20 @@ feature "Issues" do
     expect(page).to have_text(issue1.description)
     expect(page).to have_text(issue2.summary)
     expect(page).to have_text(issue2.description)
+
+    expect(page).to have_link('Create', href: new_issue_path)
+  end
+
+  scenario "Visit new issue path and fill in form" do
+    visit new_issue_path
+
+    select "New Feature", from: "issue_type_id"
+    fill_in "issue_summary", with: Faker::Name.name
+    fill_in "issue_description", with: Faker::Lorem.sentence
+    select "Medium", from: "issue_priority_id"
+    fill_in "issue_estimate_time", with: 8
+    select "Sprint 1", from: "issue_sprint_id"
+
+    expect(page).to have_button("Create")
   end
 end
