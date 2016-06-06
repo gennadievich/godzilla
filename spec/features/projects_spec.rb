7ae127786d9e74c3d25272696aa2b9085814cc24
Kirill Kovalenko <kkovalenko@beltechsoftware.com>
2016-06-06 16:47:33 +0300

    #10 Create new project spec.

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -10,4 +10,23 @@ feature "Projects" do
     expect(page).to have_text(project1.key)
     expect(page).to have_text(project1.url)
   end
+
+  scenario "Visit new project path and create project" do
+    visit new_project_path
+
+    name = Faker::Name.name
+    key = name.capitalize
+    url = Faker::Internet.url
+
+    fill_in "name", with: name
+    fill_in "key", with: key
+    select "Software", from: "project_type_id"
+    fill_in "url", with: url
+
+    click_button "Create"
+
+    expect(page).to have_text(name)
+    expect(page).to have_text(key)
+    expect(page).to have_text(url)
+  end
 end
